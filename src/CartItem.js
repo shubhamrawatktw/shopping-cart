dc479ef940b231e21606b311b8dcebf8e9aba187
shubham <shubhamrawatktw@gmail.com>
2022-05-10 18:31:40 +0530

    adding state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price:999,
+            title:"Mobile Phone",
+            qty:1,
+            img:""
+        }
+    }
 render() {
+    const {price,title,qty} = this.state
     return (
         <div className="cart-item">
             <div className="left-block">
@@ -9,11 +19,14 @@ render() {
             </div>
 
             <div className="right-block">
-            <div style={{fontSize:25}}>Phone</div>
-            <div  style={{color:"#777"}}> RS 999</div>
-            <div style={{color:"#777"}}>Qty : 1</div>
+            <div style={{fontSize:25}}>{title}</div>
+            <div  style={{color:"#777"}}>{price}</div>
+            <div style={{color:"#777"}}>{qty}</div>
             <div className="cart-item-actions">
                 {/* buttons */}
+                <img src="https://cdn-icons-png.flaticon.com/512/992/992683.png" className='action-icons' />
+                <img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" className='action-icons' />
+                <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" className='action-icons' />
             </div>
             </div>
         </div>
